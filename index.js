7afea1d202183236a6d338abbe40488b34a222e1
Raphaelmureithi <raphaelmureithi70@gmail.com>
2024-04-02 14:44:58 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+var num1 = 31;
+var num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let max = Math.max(10, 11, 12, 15, 20);
+console.log(max);
+
+var num3 = 12;
+var num4 = 8;
+let mod = num3 % num4;
+console.log(mod);
+
